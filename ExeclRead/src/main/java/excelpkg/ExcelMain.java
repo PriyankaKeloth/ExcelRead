cee1bc7f5e62baf746cb6160e73743b3e586fc60
Ranil <Ranil@DESKTOP-V42TCMT>
2024-07-11 20:35:11 +0530

    First time code uploading

---
+++ ExeclRead/src/main/java/excelpkg/ExcelMain.java
@@ -0,0 +1,23 @@
+package excelpkg;
+
+import java.io.IOException;
+
+public class ExcelMain {
+
+	public static void main(String[] args) throws IOException {
+
+		String name =Excel.readStringData(1,0);
+
+		System.out.println("Name :"+ "\t"+name);
+
+		String id= Excel.readIntegerData(1, 1);
+
+		System.out.println("id :"+"\t"+ id);
+
+
+
+		}
+
+}
+
+
